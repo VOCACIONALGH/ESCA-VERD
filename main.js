a509090db31bca032b2b479424b653017fd39e85
VOCACIONALGH <Cc20230806vocacional@hotmail.com>
2025-12-29 14:52:50 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,20 +1,20 @@
+const openCameraButton = document.getElementById("openCamera");
 const video = document.getElementById("video");
-const openButton = document.getElementById("openCamera");
 
-openButton.addEventListener("click", async () => {
+openCameraButton.addEventListener("click", async () => {
   try {
     const stream = await navigator.mediaDevices.getUserMedia({
       video: {
         facingMode: { ideal: "environment" },
-        width: { ideal: 9999 },
-        height: { ideal: 9999 }
+        width: { ideal: 4096 },
+        height: { ideal: 2160 }
       },
       audio: false
     });
 
     video.srcObject = stream;
-    await video.play();
-  } catch (err) {
-    console.error("Erro ao abrir a câmera:", err);
+
+  } catch (error) {
+    console.error("Erro ao abrir a câmera:", error);
   }
 });
